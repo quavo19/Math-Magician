1dd69d6589056768852f00639e9f02a477f852c4
Donald Akite <donaldakite27@gmail.com>
2023-01-19 01:41:30 +0000

    Add routs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './Style.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Calculator from './Components/Calculator';
+import Navbar from './Components/Nav';
+import HomePage from './Components/Home';
+import Quotes from './Components/Quotes';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quotes" element={<Quotes />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
   );
 }
 
