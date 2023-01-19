7b8297358e1dcd02df73bd35fbab821f61413526
Donald Akite <donaldakite27@gmail.com>
2023-01-19 01:39:26 +0000

    Add a home page

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className="Home">
+    <h3>Math Magicians</h3>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing
+      elit. Ullam nam est explicabo nesciunt labore odit ex pers
+      piciatis tempore maxime voluptatem molestiae, earum exercitationem
+      unde iste porro cupiditate pariatur minima nulla.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Laudantium facere, iure beatae eveniet illo veniam quisquam,
+      alias nostrum earum perferendis ab fugit praesentium sed eum
+      architecto blanditiis repellendus nesciunt saepe.
+
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing
+      elit. Ullam nam est explicabo nesciunt labore odit ex pers
+      piciatis tempore maxime voluptatem molestiae, earum exercitationem
+      unde iste porro cupiditate pariatur minima nulla.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Laudantium facere, iure beatae eveniet illo veniam quisquam,
+      alias nostrum earum perferendis ab fugit praesentium sed eum
+      architecto blanditiis repellendus nesciunt saepe.
+
+    </p>
+  </div>
+);
+
+export default HomePage;
